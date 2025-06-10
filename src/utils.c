8bbd0ffd1db061c12de53f97563a89e26e0996d9
t3ssnim <tessnim.elyaakoubi@etu.estia.fr>
2025-06-10 10:18:15 +0200

    modified:   .vscode/settings.json modified:   src/features.h modified:   src/utils.c modified:   src/utils.h

--- src/utils.c
+++ src/utils.c
@@ -3,7 +3,12 @@
 #include "utils.h"
 
 pixelRGB * get_pixel( unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y ){
-    
+    return (pixelRGB*)&data[(y * width + x) * 3];
+}
+
+void print_pixel(unsigned int width, unsigned x, unsigned y) {
+    pixelRGB *pixel = get_pixel(data, width, height, x, y, n);
+    printf("Pixel (%d, %d): R=%d, G=%d, B=%d\n", x, y, pixel->R, pixel->G, pixel->B)
 }
 
 int setPixel(){
