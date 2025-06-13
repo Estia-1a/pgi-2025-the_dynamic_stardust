2fa88f6435cbda7a5e1641952089d3918bec4dc4
AUBERTVictor <victor.aubert@etu.estia.fr>
2025-06-13 17:16:42 +0200

    correction dimension modified:   src/features.c

--- src/features.c
+++ src/features.c
@@ -17,8 +17,8 @@ void helloWorld() {
 
 void dimension (char *source_path){
     int width, height, channel_count, m ;
-    unsigned char *data;
-    m=read_image_data(source_path,*data,width,height,channel_count);
+    unsigned char **data;
+    m=read_image_data(source_path,data,&width,&height,&channel_count);
     if (m==0){
         perror("Erreur de lecture de l'image");
     }
