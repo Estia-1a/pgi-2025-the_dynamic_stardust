87525b31d381ec8b0bab3bfdb6a00628f6128c54
AUBERTVictor <victor.aubert@etu.estia.fr>
2025-06-13 15:31:23 +0200

    résolutions d'une petite partie des problèmes de Second_line modified:   src/utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,28 +1,28 @@
 #include <estia-image.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "utils.h"
 
 pixelRGB * get_pixel( unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y ) {
-
-    }
-
     return (pixelRGB *)&data[index];
 }
 
 void print_pixel(const char *filename, int x, int y) {
-    unsigned char *data; int width, height, n;
+    unsigned char **data; int width, height, n;
     if (read_image_date(filename, &data, &width, &height, &n)) return;
     pixelRGB *pixel = get_pixel(data, width, height, x, y, n);
     if (pixel) printf("Pixel (%d, %d): R=%d, G=%d, B=%d\n", x, y, pixel->R, pixel->G, pixel->B);
-    free(data);
+    free(*data);
 }
 
 void second_line(char *source_path){
-    int height, width,m ;
-    unsigned char *data ;
-    m= read_image_data(const char *filename, unsigned char **data, int *width, int *height, int *channel_count) ;
+    int height, width,m,channel_count ;
+    unsigned char **data ;
+    m= read_image_data(source_path, *data,width,height,channel_count) ;
     if (m==0){
-        fprintf("Erreur lecture de l'image");
+        perror("Erreur lecture de l'image");
     }
     else {
         printf("second_line: %d, %d, %d", *data[3*width],*data[3*width+1],data[3*width+2]) ;
