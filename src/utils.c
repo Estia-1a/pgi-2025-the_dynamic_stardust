d4fa8b9919ebdb348cf3e1a5dcf7cb4c14f3b1ae
t3ssnim <tessnim.elyaakoubi@etu.estia.fr>
2025-06-13 15:36:56 +0200

    modified:   src/utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,6 +1,8 @@
 #include <estia-image.h>
 #include <stddef.h>
 #include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 pixelRGB * get_pixel( unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y ) {
     if (x >= width || y >= height) {
@@ -23,11 +25,12 @@ void print_pixel(char *filename, int x, int y) {
     unsigned char *data
     int rid, width, height, channel_count;
     rid=read_image_data(filename, *data, width, height, channel_count);
-    if (get_pixel) {
+    if (rid == NULL) {
         return NULL;
     }
-    else if (rid == NULL){
-        return NULL;
+    pix = pixelRGB * get_pixel( *data, width, height, n, x, y);
+    else if (pix==NULL){
+        return;
     }
     else{
         printf("print_pixel (%d, %d): %d, %d, %d\n", x, y, R, G, B);
